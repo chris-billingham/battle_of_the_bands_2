620654e55b5b4f3566d86e4fdbc7377d7ffc60fe
Chris Billingham <chris.billingham@gmail.com>
2020-01-04 18:09:17 +0000

    that seems to do the single scrape and tidy

--- R/01_scrape-charts.R
+++ R/01_scrape-charts.R
@@ -4,3 +4,15 @@ library(polite)
 
 url <- "https://www.officialcharts.com/charts/singles-chart/19940102/7501/"
 
+test <- read_html(url) %>% html_node("table.chart-positions") %>% html_table(header = TRUE) %>% 
+  select(1:5) %>%
+  filter(!grepl("google", `Title, Artist`))
+
+rows <- seq(1, nrow(test), by = 5)
+
+df <- test[rows,]
+
+df$`Title, Artist` <- gsub("\n", "", df$`Title, Artist`)
+
+df2 <- df %>% separate(`Title, Artist`, into = c("title", "artist", "label"), sep = "( ){2,}")
+
