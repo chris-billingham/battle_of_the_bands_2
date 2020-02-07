e7f6e5ca90de9682843d4930b8f4e54ae7388bb4
Chris Billingham <chris.billingham@gmail.com>
2020-02-07 21:17:13 +0000

    this is getting weird

--- 02_snag-the-lyrics.R
+++ 02_snag-the-lyrics.R
@@ -11,10 +11,19 @@ new_lyrics <- map2_dfr(charts_new_binned$artist,
                        info = "all")
 
 # oh crap some don't exist, better use possible_lyrics
-new_lyrics <- map2_dfr(charts_new_binned$artist, 
-                       charts_new_binned$title, 
+new_lyrics <- map2_dfr(charts_new_binned$artist[1:10], 
+                       charts_new_binned$title[1:10], 
                        possible_lyrics, 
                        info = "all")
 
+# i need something better actually, i need to see how many are missing
+lyrics_possible <- possibly(genius_lyrics, 
+                            otherwise = tibble(artist = "missing",
+                                                  track_title = "missing"))
 
+# right, third time lucky
+new_lyrics <- map2_dfr(charts_new_binned$artist, 
+                       charts_new_binned$title, 
+                       lyrics_possible, 
+                       info = "all")
 
